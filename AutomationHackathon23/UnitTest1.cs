70140bb2c20cc0bf5a78a51be4ab7e7283b59419
Vytautas Matulevicius <vytautas.matulevicius@devbridge.com>
2023-07-27 15:32:42 +0300

    Our own created lyrics and song

--- AutomationHackathon23/UnitTest1.cs
+++ AutomationHackathon23/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Speech.Synthesis;
+
 namespace AutomationHackathon23
 {
     public class Tests
@@ -15,27 +17,6 @@ namespace AutomationHackathon23
         {
             chrome.Quit();
             chrome.Dispose();
-        }
-
-        [Test]
-        public void Test1()
-        {
-            chrome.Navigate().GoToUrl("http://www.google.com");
-
-            var acceptButton = chrome.FindElement(By.XPath(".//button[.//div[normalize-space()='Priimti viską']]"));
-            acceptButton.Click();
-
-            var searchInput = chrome.FindElement(By.Name("q"));
-            searchInput.SendKeys("devbridge");
-            Thread.Sleep(500);
-            searchInput.SendKeys(Keys.Enter);
-            Thread.Sleep(500);
-
-            var searchResults = chrome.FindElements(By.XPath(".//div[@class='g']"));
-            var searchResult = searchResults[0];
-            var link = searchResult.FindElement(By.CssSelector("a"));
-
-            Assert.True(link.GetAttribute("href").Contains("devbridge.com"));
         }
 
         [Test]
@@ -61,6 +42,78 @@ namespace AutomationHackathon23
         [Test]
         public void Task5()
         {
+            
+        string lyrics = "Yo we're here in Hackathon's scene\n" +
+                       "Agota's smart Gintare coding queen\n" +
+                       "Edvinas plans Veetoutas schemes\n" +
+                       "A dream team fearless and keen\n" +
+                       "In the hood we stand tall no doubt\n" +
+                       "Obstacles vanish we route 'em out\n" +
+                       "Gangsta coders on a mission true\n" +
+                       "Bringing fire coding miracles we do\n" +
+                       "The baddest we're quick none sicker\n" +
+                       "Hackathon kings none's quicker\n" +
+                       "Sucker don't mess we run the show\n" +
+                       "Coding flow we steal the glow\n" +
+                       "In coding dojo we raise the bar\n" +
+                       "Agota's algorithms a guiding star\n" +
+                       "Gintare's UIs like art they are\n" +
+                       "Edvinas' tactics precision par\n" +
+                       "Veetoutas hustles never rests\n" +
+                       "Innovating we're the best\n" +
+                       "No challenge big we're on the quest\n" +
+                       "Arena skills put to test\n" +
+                       "The baddest we're quick none sicker\n" +
+                       "Hackathon kings none's quicker\n" +
+                       "Sucker don't mess we run the show\n" +
+                       "Coding flow we steal the glow\n" +
+                       "Late-night brainstorm ignite\n" +
+                       "Hand in hand expand with might\n" +
+                       "Agota's insights shine so bright\n" +
+                       "Gintare's designs pure delight\n" +
+                       "Edvinas' strategies masterstroke\n" +
+                       "Veetoutas' hustle never broke\n" +
+                       "Collaboration our secret tool\n" +
+                       "Together we rule break the rule\n" +
+                       "The baddest we're quick none sicker\n" +
+                       "Hackathon kings none's quicker\n" +
+                       "Sucker don't mess we run the show\n" +
+                       "Coding flow we steal the glow\n" +
+                       "Agota Gintare Edvinas Veetoutas the crew\n" +
+                       "Broke through Hackathon courage we drew\n" +
+                       "Intelligence and might we grew\n" +
+                       "Together conquer any height it's true.";
+        
+            string[] lines = lyrics.Split('\n');
+            
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            int barTimeMilliseconds = 5;
+
+            try
+            {
+                chrome.Navigate().GoToUrl("https://chordu.com/chords-tabs-50-cent-if-i-can-t-official-instrumental-hq-id_bwW7WY0rRXE");
+                
+                IWebElement hourglassPlayButton = chrome.FindElement(By.XPath("//*[@id='hourglass_play_btn']"));
+                hourglassPlayButton.Click();
+
+                System.Threading.Thread.Sleep(650);
+
+                foreach (string line in lines)
+                {
+                    synthesizer.Speak(line);
+                    Thread.Sleep(barTimeMilliseconds);
+                }
+                
+                chrome.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                synthesizer.Dispose();
+            }
         }
 
         [Test]
@@ -73,5 +126,4 @@ namespace AutomationHackathon23
         {
         }
     }
-// pyrst
 }
