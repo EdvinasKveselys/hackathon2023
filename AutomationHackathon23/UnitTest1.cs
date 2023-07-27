02df6a90cb38ac3a92b9c748d90f3f4db81ec732
Edvinas Kveselys <edvinas.kveselys@devbridge.com>
2023-07-27 15:25:47 +0300

    Task2

--- AutomationHackathon23/UnitTest1.cs
+++ AutomationHackathon23/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework.Constraints;
+using OpenQA.Selenium.Interactions;
 
 namespace AutomationHackathon23
 {
@@ -48,6 +49,97 @@ namespace AutomationHackathon23
         [Test]
         public void Task2()
         {
+            chrome.Navigate().GoToUrl("https://www.mathster.com/10secondsmaths/");
+
+            var checkboxes = chrome.FindElements(By.XPath(".//input[@type='checkbox']"));
+            for (int i = 1; i < checkboxes.Count; i++)
+            {
+                var checkbox = checkboxes[i];
+                checkbox.Click();
+            }
+            var slider = chrome.FindElement(By.XPath(".//div[@class='tooltip']"));
+            new Actions(chrome).ClickAndHold(slider).MoveByOffset(250, 0).Release().Build().Perform();
+            try
+            {
+                while (!chrome.FindElement(By.XPath(".//div[@id='results']")).Displayed)
+                {
+                    if (chrome.FindElement(By.XPath(".//div[@id='results']")).Displayed)
+                    {
+                        break;
+                    }
+
+                    var question = chrome.FindElement(By.XPath(".//p[@id='question']"));
+                    var questionText = question.Text;
+
+                    string[] split;
+                    double first;
+                    double second;
+                    double result = 0;
+
+                    if (questionText.Contains("+"))
+                    {
+                        split = questionText.Split("+");
+                        first = int.Parse(split[0]);
+                        second = int.Parse(split[1]);
+                        result = first + second;
+                    }
+
+                    if (questionText.Contains("-"))
+                    {
+                        split = questionText.Split("-");
+                        first = int.Parse(split[0]);
+                        second = int.Parse(split[1]);
+                        result = first - second;
+                    }
+
+                    if (questionText.Contains("÷"))
+                    {
+                        split = questionText.Split("÷");
+                        first = int.Parse(split[0]);
+                        second = int.Parse(split[1]);
+                        result = first / second;
+                    }
+
+                    if (questionText.Contains("×"))
+                    {
+                        split = questionText.Split("×");
+                        first = int.Parse(split[0]);
+                        second = int.Parse(split[1]);
+                        result = first * second;
+                    }
+
+                    if (questionText.Contains("√"))
+                    {
+                        split = questionText.Split("√");
+                        second = double.Parse(split[1]);
+                        result = Math.Sqrt(second);
+                    }
+
+                    if (questionText.Contains("²"))
+                    {
+                        split = questionText.Split("²");
+                        second = double.Parse(split[0]);
+                        result = Math.Pow(second, 2);
+                    }
+
+                    if (questionText.Contains("³"))
+                    {
+                        split = questionText.Split("³");
+                        second = double.Parse(split[0]);
+                        result = Math.Pow(second, 3);
+                    }
+
+                    var answerBow = chrome.FindElement(By.XPath(".//input[@id='question-answer']"));
+                    answerBow.SendKeys(result.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Thread.Sleep(5000);
+            }
+            
+            Thread.Sleep(5000);
         }
 
         [Test]
@@ -92,7 +184,7 @@ namespace AutomationHackathon23
                     cells[lol].Click();
                 }
             }
-
+            
             Thread.Sleep(5000);
         }
     }
